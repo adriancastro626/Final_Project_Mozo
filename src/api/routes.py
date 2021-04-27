de8c965bc5d5bc4cc2a1a027d30498b790abee7b
Gineth <ginysp@hotmail.com>
2021-04-27 04:29:22 +0000

    include getorders

--- src/api/routes.py
+++ src/api/routes.py
@@ -3,7 +3,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 import os
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Order, OrderType
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
@@ -34,3 +34,8 @@ def get_hello():
     }
     
     return jsonify(dictionary)
+
+@api.route('/manageorder', methods=['GET'])
+#@jwt_required()
+def get_AllOrders():      
+    return jsonify(Order.getAllOrders()), 200
