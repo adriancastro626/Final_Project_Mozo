ef81128bb7d105d5e8cb062904903e98804c8ddc
Gineth <ginysp@hotmail.com>
2021-05-02 18:44:05 +0000

    homa page ready

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -19,77 +19,65 @@ export const Home = () => {
 		<div className="cont s--inactive">
 			<div className="cont__inner">
 				<div className="el">
-					<div>
-						<Link to="/frontmenu">
-							<Button label="Menu" className="p-button-rounded p-button-secondary" />
-						</Link>
-					</div>
-					<div className="el__overflow">
-						<div className="">
-							<div className="el__bg" />
+					<Link to="/managemenu">
+						<div className="el__overflow">
 							<div className="">
-								<img src="https://i.ibb.co/3BvpXJN/menu.jpg" />
+								<div className="el__bg" />
+								<div>
+									<img src="https://i.ibb.co/3BvpXJN/menu.jpg" />
+								</div>
 							</div>
 						</div>
-					</div>
-					<div className="el__index">
-						<div className="el__index-back">1</div>
-						<div className="el__index-front">
-							<div className="el__index-overlay" data-index="1">
-								1
+						<div className="el__index">
+							<div className="el__index-back">1</div>
+							<div className="el__index-front">
+								<div className="el__index-overlay" data-index="1">
+									1
+								</div>
 							</div>
 						</div>
-					</div>
+					</Link>
 				</div>
 				<div className="el">
-					<div>
-						<Link to="/manageorder">
-							<Button label="Ordenes" className="p-button-rounded p-button-secondary" />
-						</Link>
-					</div>
-					<div className="el__overflow">
-						<div className="">
-							<div className="el__bg" />
+					<Link to="/manageorder">
+						<div className="el__overflow">
 							<div className="">
-								<Link to="/manageorder">
+								<div className="el__bg" />
+								<div>
 									<img src="https://i.ibb.co/bHSNyYz/ordenes.jpg" />
-								</Link>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div className="el__index">
-						<div className="el__index-back">2</div>
-						<div className="el__index-front">
-							<div className="el__index-overlay" data-index="2">
-								2
+						<div className="el__index">
+							<div className="el__index-back">2</div>
+							<div className="el__index-front">
+								<div className="el__index-overlay" data-index="2">
+									2
+								</div>
 							</div>
 						</div>
-					</div>
+					</Link>
 				</div>
+
 				<div className="el">
-					<div>
-						<Link to="/register">
-							<Button label="Usuario" className="p-button-rounded p-button-secondary" />
-						</Link>
-					</div>
-					<div className="el__overflow">
-						<div className="">
-							<div className="el__bg" />
+					<Link to="/register">
+						<div className="el__overflow">
 							<div className="">
-								<Link to="/register">
+								<div className="el__bg" />
+								<div>
 									<img src="https://i.ibb.co/MkjbMrs/managing-users.jpg" />
-								</Link>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div className="el__index">
-						<div className="el__index-back">3</div>
-						<div className="el__index-front">
-							<div className="el__index-overlay" data-index="3">
-								3
+						<div className="el__index">
+							<div className="el__index-back">3</div>
+							<div className="el__index-front">
+								<div className="el__index-overlay" data-index="3">
+									3
+								</div>
 							</div>
 						</div>
-					</div>
+					</Link>
 				</div>
 			</div>
 		</div>
